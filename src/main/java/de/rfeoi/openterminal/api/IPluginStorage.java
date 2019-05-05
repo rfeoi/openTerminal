8ad2f144384443f914d93710a46a90b9a5518f50
Felix <felixletsplay@outlook.de>
2019-05-05 21:58:25 +0200

    Add Methods isBlockCapable(...) and playerCanNotPlace(...) and tick
    isBlockCapableToHandleSystem will check if router block is placable there
    playerCanNotPlaceSystemHere should check if the player is able to place the block and return (possible) error message
    tick is called on every tileEntity tick

--- src/main/java/de/rfeoi/openterminal/api/IPluginStorage.java
+++ src/main/java/de/rfeoi/openterminal/api/IPluginStorage.java
@@ -1,9 +1,18 @@
 package de.rfeoi.openterminal.api;
 
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public interface IPluginStorage {
+
+    /**
+     * Tick method, will be called by tick of tileEntity of Router
+     */
+    default void tick() {}
+
     /**
      * Get Items stored in Storage
      * @return Items stored in Storage
@@ -30,4 +39,28 @@ public interface IPluginStorage {
      * @return if the system is able to craft this
      */
     boolean craft(ItemStack itemStack);
+
+    /**
+     * Checks if system is able to connect to router block
+     * @param pos of possible router block
+     * @param worldIn of possible router block
+     * @return if there is any connection to the system
+     */
+    static boolean isBlockCapableToHandleSystem(BlockPos pos, World worldIn){
+        return false;
+    }
+
+    /**
+     * Checks if there are any other (security issues like Security Terminal) for placing block
+     * Only if isBlockCapableToHandleSystem is true
+     *
+     * @param pos of possible router block
+     * @param worldIn of possible router block
+     * @param player who tries to place the block
+     * @return Not able to place message. IMPORTANT: If there is no Error return null (not empty string) only then it is possible to place router block
+     */
+    static String playerCanNotPlaceSystemHere(BlockPos pos, World worldIn, EntityPlayer player) {
+        return "Block not fully configured yet. Please contact Mod author.";
+    }
+
 }
